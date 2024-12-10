693b53698c8e93dc4a93ffedea3e64b95d60a5b1
mac <mac@macs-MacBook-Pro.local>
2024-12-11 00:21:40 +0100

    Average done

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,31 @@
+import seed
+import mysql.connector
+from decimal import Decimal
+
+def stream_user_ages():
+    query = '''
+        SELECT age from user_data;
+    '''
+    connect_to_database = seed.connect_to_prodev()
+    try:
+        if connect_to_database and connect_to_database.is_connected():
+            with connect_to_database.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                for row in result:
+                    age = row[0]
+                    yield age
+    except mysql.connector.Error as err:
+        return (f"Error is {err}")
+
+def check_average_age():
+    result_from_query = stream_user_ages()
+    newNumber = 0
+    average = sum(item for item in result_from_query if item is not None)/ 2
+    print(average)
+
+
+
+
+check_average_age()
+#stream_user_ages()
