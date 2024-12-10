f5192ce8aed46f8d71a730ebad5b3c97743d3e97
mac <mac@macs-MacBook-Pro.local>
2024-12-10 22:17:39 +0100

    Updated

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,16 +1,20 @@
 import seed
+import mysql.connector
 
 def stream_users_in_batches(batch_size):
     query = f'''
                 SELECT * FROM user_data LIMIT {batch_size};
             '''
     connection_to_database = seed.connect_to_prodev()
-    if connection_to_database and connection_to_database.is_connected():
-        with connection_to_database.cursor() as cursor:
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            for row in rows:
-                yield row
+    try:
+        if connection_to_database and connection_to_database.is_connected():
+            with connection_to_database.cursor() as cursor:
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                for row in rows:
+                    yield row
+    except mysql.connector.Error as err:
+        return (f"Error is {err}")
             
 
 
@@ -19,12 +23,15 @@ def batch_processing(batch_size):
                 SELECT * FROM user_data WHERE user_data.age > 25 LIMIT {batch_size};
             '''
     connection_to_database = seed.connect_to_prodev()
-    if connection_to_database and connection_to_database.is_connected():
-        with connection_to_database.cursor() as cursor:
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            for row in rows:
-                yield row
+    try:
+        if connection_to_database and connection_to_database.is_connected():
+            with connection_to_database.cursor() as cursor:
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                for row in rows:
+                    yield row
+    except mysql.connector.Error as err:
+        return (f"Error is {err}")
 
 
 stream_users_in_batches(5)
