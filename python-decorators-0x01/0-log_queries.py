76428c48929b0ca52bbbc61bd499c388240fb6c5
mac <mac@macs-MacBook-Pro.local>
2024-12-11 11:30:32 +0100

    Updated: Db_connection decorator

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,11 +1,6 @@
 import sqlite3
-import functools
 from datetime import datetime
-import seed
-
-
 #### decorator to log SQL queries
-
 def log_queries(func):
     def wrapper(*args, **kwargs):
         print(f'''The query is: {kwargs}\n
@@ -14,18 +9,9 @@ def log_queries(func):
         return func(*args, **kwargs)
     return wrapper
 
-'''@log_queries
-def fetch_all_users(query):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    conn.close()
-    return results '''
-
 @log_queries
 def fetch_all_users(query):
-    conn = seed.connect_to_prodev()
+    conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
