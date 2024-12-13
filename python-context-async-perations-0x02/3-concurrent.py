a4fd16425430755d8d956845497ee57744fcc4a9
mac <mac@macs-MacBook-Pro.local>
2024-12-13 13:59:49 +0100

    Update Done

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,8 +1,8 @@
 import aiosqlite
 import asyncio
 
-async def async_fetch_users(database):
-    async with aiosqlite.connect(database=database) as asynccursor:
+async def async_fetch_users():
+    async with aiosqlite.connect('users.db') as asynccursor:
         query = "SELECT * FROM users"
         async with asynccursor.execute(query) as cursor:
             allData = await cursor.fetchall()
@@ -10,8 +10,8 @@ async def async_fetch_users(database):
                 print(row)
 
 
-async def async_fetch_older_users(database):
-    async with aiosqlite.connect(database=database) as asynccursor:
+async def async_fetch_older_users():
+    async with aiosqlite.connect('users.db') as asynccursor:
         query = "SELECT * FROM users where age > 40"
         async with asynccursor.execute(query) as cursor:
             allData = await cursor.fetchall()
@@ -19,8 +19,8 @@ async def async_fetch_older_users(database):
                 print(row)
 
 async def fetch_concurrently():
-    result = await asyncio.gather(async_fetch_users('users.db'
-    ), async_fetch_older_users('users.db'))
+    result = await asyncio.gather(async_fetch_users(), 
+    async_fetch_older_users())
     return result
 
 asyncio.run(fetch_concurrently())
