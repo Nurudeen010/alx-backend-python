9f0ea72bd2ef3cb94105cfc8e1929953cf957938
mac <mac@macs-MacBook-Pro.local>
2024-12-22 17:17:57 +0100

    feat: Urls configured for the end points

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
+from rest_framework_nested.routers import NestedDefaultRouter
 
 router = routers.DefaultRouter()
 router.register(r'conversation', ConversationViewSet, basename='conversation')
-router.register(r'message', MessageViewSet, basename='message')
+
+
+#Nested router for messages under conversation
+conversation_router = NestedDefaultRouter(router, r'conversation', lookup='conversation')
+conversation_router.register(r'message', MessageViewSet, basename='conversation-message')
 
 urlpatterns = [
     path('', include(router.urls)),
+     path('', include(conversation_router.urls)),
 ]
