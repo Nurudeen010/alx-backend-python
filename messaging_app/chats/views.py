0cfbf3a5a72abb6c7740597f3d0372140c4add00
mac <mac@macs-MacBook-Pro.local>
2024-12-22 16:53:12 +0100

    Urls Added

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,12 +1,20 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, status
 from .serializers import conversation, message
 from .models import Conversation, Message
+from rest_framework.response import Response
 
 
-class ConversationViewSet(ModelViewSet):
-    queryset = Conversation.objects.all()
-    serializer_class = conversation
+class ConversationViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Conversation.objects.all()
+        serializer = conversation(queryset, many=True)
+        response = serializer.data
+        return Response(response)
+        
+        
 
-class MessageViewSet(ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = message
+class MessageViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Message.objects.all()
+        serializer = message(queryset, many=True)
+        return Response(serializer.data)
